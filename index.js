6d07de7833d561fa49284140568e5a33c3e909c7
Christian Smith <th3h3ro23@gmail.com>
2018-05-09 16:00:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+function shout(string) {
+  return string.toUpperCase()
+}
+// correct
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+// correct
+
+function sayHiToGrandma(string) {
+  console.log("HELLO!")
+}
